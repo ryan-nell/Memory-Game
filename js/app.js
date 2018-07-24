0a16871ba10f2a80eddb68c01e05d9e503b87755
Ryan Nell <ryannell@hotmail.com>
2018-07-24 16:45:06 +0100

    shuffle card placement

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
+
+const card = document.querySelectorAll('.card');
 const cardBody = document.querySelectorAll('.card-body');
+const cardsArray = [], shuffleArray = [];
+
 
 for(let i = 0; i < cardBody.length; i++){
-  cardBody[i].addEventListener('click', function() {
-    cardBody[i].classList.toggle('flipped');
+  cardBody[i].remove();
+  while(shuffleArray.length < 16){
+    let randomNum = Math.floor(Math.random() * card.length);
+    if(shuffleArray.indexOf(randomNum) > -1) continue;
+    shuffleArray[shuffleArray.length] = randomNum;
+    cardsArray.push(cardBody[randomNum]);
+  }
+}
+
+for(let j = 0; j < cardsArray.length; j++){
+  card[j].append(cardsArray[j]);
+  cardBody[j].addEventListener('click', function() {
+    cardBody[j].classList.toggle('flipped');
   });
 }
