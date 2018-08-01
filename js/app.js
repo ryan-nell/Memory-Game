241f95dbf63092231d92f7350b28b45e691ff224
Ryan Nell <ryannell@hotmail.com>
2018-08-01 21:49:01 +0100

    Convert card shuffle loops into a function

--- js/app.js
+++ js/app.js
@@ -2,23 +2,33 @@
 const card = document.querySelectorAll('.card');
 const cardBody = document.querySelectorAll('.card-body');
 const cardImage = document.querySelectorAll('i');
-let deckOfCards = [], shuffleCards = [];
+let deckOfCards = [], shuffleCards = [], matchCards = [];
+let firstCard, secondCard;
 
+//shuffle the cards when the page loads
+document.body.onload = shuffleDeckOfCards();
 
-for(let singleCard in cardBody){
-  while(shuffleCards.length < 16){
-    let randomNum = Math.floor(Math.random() * card.length);
-    if(shuffleCards.indexOf(randomNum) > -1){
-      continue;
+//function to shuffle cards
+function shuffleDeckOfCards(){
+  //loop using length of cardbody
+  for(let singleCard in cardBody){
+    while(shuffleCards.length < 16){
+      //use math.random to generate a random numbers up to 16
+      let randomNum = Math.floor(Math.random() * card.length);
+      //Make sure numbers are unique
+      if(shuffleCards.indexOf(randomNum) > -1){
+        continue;
+      }
+      shuffleCards[shuffleCards.length] = randomNum;
+      deckOfCards.push(cardBody[randomNum]);
     }
-    shuffleCards[shuffleCards.length] = randomNum;
-    deckOfCards.push(cardBody[randomNum]);
   }
-}
 
-for(let cards in deckOfCards){
-  card[cards].append(deckOfCards[cards]);
-  cardBody[cards].addEventListener('click', function() {
-    cardBody[cards].classList.toggle('flipped');
-  });
+  // loop through each card within the deckOfCards array and append the newly
+  // shuffled cards to the card container
+  for(let cards in deckOfCards){
+    //Add shuffled cards to the card containers
+    card[cards].append(deckOfCards[cards]);
+  }
+  console.log(deckOfCards);
 }
